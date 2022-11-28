d3ea53b5e4c9f1b7fdca42c6e23f5e45023df771
jdev <20902932+osekojoe@users.noreply.github.com>
2022-11-28 18:01:00 +0000

    sys stderr write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-try:
-	sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
-finally:
-	sys.exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)

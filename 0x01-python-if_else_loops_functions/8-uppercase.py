6b605f5f8cfc33f980465c1df056b08353d769bb
jdev <20902932+osekojoe@users.noreply.github.com>
2022-11-29 18:02:02 +0000

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            addendum = 32
+        else:
+            addendum = 0
+        print(":c".format(ord(str[i]) - 32), end='')
+    print()

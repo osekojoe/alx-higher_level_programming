4377c45e611d0897422a4579bfe7e418fa5b9f6d
jdev <20902932+osekojoe@users.noreply.github.com>
2022-11-29 17:04:42 +0000

    different two digit combinations

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 90):
+    if i % 10 > i /10:
+        if i != 89:
+            print("{0:0=2d}, ".format(i), end='')
+        else:
+            print('{0:0=2d}'.format(i))

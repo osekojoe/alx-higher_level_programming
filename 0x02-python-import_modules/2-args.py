0b8cb01fd62fadd09c45304e82dabb365a469d5e
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-01 10:34:47 +0000

    print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    nargs = len(sys.argv) - 1
+
+    if nargs == 0:
+        print("{} arguments:".format(nargs))
+    elif nargs == 1:
+       print("{} argument:".format(nargs))
+    else:
+       print("{} arguments:".format(nargs))
+
+    if nargs >= 1:
+       nargs = 0
+       for arg in sys.argv:
+           if nargs != 0:
+               print("{}: {}".format(nargs, arg))
+           nargs += 1

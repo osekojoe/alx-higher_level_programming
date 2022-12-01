159d7bdb9247fc19d6852086987f1e1e0e2b2f73
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-01 10:38:44 +0000

    print args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
        print("{} arguments:".format(nargs))
 
     if nargs >= 1:
-       nargs = 0
-       for arg in sys.argv:
-           if nargs != 0:
-               print("{}: {}".format(nargs, arg))
-           nargs += 1
+        nargs = 0
+        for arg in sys.argv:
+            if nargs != 0:
+                print("{}: {}".format(nargs, arg))
+            nargs += 1

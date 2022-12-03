a024377b943966ede3efc7c76ea8c87a398079c5
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-03 14:18:57 +0000

    print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if col != 0:
+                print(' ', end='')
+            print("{:d}".format(matrix[row][col]), end='')
+        print()

449979648c2dfee0827733ec12479085125e3448
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-04 11:19:13 +0000

    add multiples of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,8 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            my_list.append(True)         
+            new_list.append(True)         
         else:
-            my_list.append(False)
+            new_list.append(False)
 
     return new_list

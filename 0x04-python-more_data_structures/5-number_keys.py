2e725a785383c400b69f4970f0305ad4ebd5e01b
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-07 15:38:17 +0000

    count dict keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     num = 0
-    key_lists = list(a_dictionary.keys())
+    keys_list = list(a_dictionary.keys())
 
     for i in keys_list:
         num += 1

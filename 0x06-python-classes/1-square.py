4d50ca4b246b5047047f7438a81f55c2107b3464
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-20 17:02:12 +0000

    instantiate class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size):    
+        '''initialize a square'''
+        self.__size = size

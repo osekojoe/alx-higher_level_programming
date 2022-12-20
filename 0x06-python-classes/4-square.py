c118a34466d54b9c1ba808cfaf0bd3353aac79c4
jdev <20902932+osekojoe@users.noreply.github.com>
2022-12-20 17:50:42 +0000

    access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Define class Square."""
 
 
 class Square:
@@ -9,23 +9,23 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
-            size (int): size of square.
+            size (int): size of square
         """
         self.size = size
 
-        @property
-        def size(self):
-            """get size of square"""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """area of square"""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return area of the square."""
+        return (self.__size * self.__size)

1486e7ee9f68152e484eab71350c4558ec4f6634
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-04 12:59:25 +0000

    numpy multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """ Matrix multiplication"""
 
+
 import numpy as np
+
 
 def lazy_matrix_mul(m_a, m_b):
     """ Matrix multiplication with numpy
     Args:
         m_a: first matrix
-        m_b: second matrix        
+        m_b: second matrix
     Return:
         Matrix
     """

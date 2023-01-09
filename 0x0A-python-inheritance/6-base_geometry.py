13951059c36dbe8d7dbbe472b8402d5c9d7a4e36
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-09 16:31:29 +0000

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+empty class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Public instance method: def area(self):
+    raises an Exception; message area() is not implemented"""
+    def area(self):
+        """raise exeption"""
+        raise Exception("area() is not implemented")

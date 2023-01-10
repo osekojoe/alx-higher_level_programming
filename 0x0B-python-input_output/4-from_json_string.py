ca8d1117e88b609024b4ec567f5f980d3596781c
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-10 16:30:40 +0000

    json str to obj

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+a function that returns an object (Python data structure)
+ represented by a JSON string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    json to obj
+    """
+    return json.loads(my_str)

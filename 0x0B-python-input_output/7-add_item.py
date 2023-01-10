4c405ee6a7e9330b80f3278839b24138ea2e2729
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-10 16:54:39 +0000

    load, add, save - json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+a script that adds all arguments to a list & save them to a file
+uses function: load_from_json_file from 6-load_from_json_file.py
+list must be saved as a JSON representation in a file named add_item.json
+"""
+
+
+import sys
+import os.path
+import json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+my_file = 'add_item.json'
+my_list = []
+
+if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
+    my_list = load_from_json_file(my_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        my_list.append(elem)
+
+save_to_json_file(my_list, my_file)

554b693c930ed50dc4e225bb8f03bdfcd84b50f9
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-10 17:49:53 +0000

    lists as pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,22 +12,23 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
+    limit = n - 1
     triangle = [[1]]
 
-    for i in range(n - 1):
+    for i in range(limit):
         row = []
-        row.append(i)
+        row.append(1)
 
         if len(triangle[i]) > 1:
-            prev_len = len(triangle[i]) - 1
+            prev_row_len = len(triangle[i]) - 1
             nxt = 1
 
-            for j in range(prev_len):
-                total = triangle[i][j] + triangle[i][nxt]
-                row.append(total)
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
                 nxt += 1
 
-        row.append(i)
+        row.append(1)
         triangle.append(row)
 
     return triangle

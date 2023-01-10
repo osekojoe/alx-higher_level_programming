c61baeda598acd429a32b3800fbefdaa162aa16c
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-10 16:27:10 +0000

    to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+a function that returns the JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    return json representation of obj
+    """
+    return json.dumps(my_obj)

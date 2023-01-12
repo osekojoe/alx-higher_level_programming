e86293b25db5b31d4605f4e162951b6014299e5e
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-12 11:25:32 +0000

    base class + init

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Class Base:
+ private class attribute __nb_objects = 0
+"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        if id is not None, assign the public instance attribute id with this
+          argument value - assumption: id is an integer (not testing needed)
+        otherwise, increment __nb_objects and assign the new value to the public
+          instance attribute id
+        """

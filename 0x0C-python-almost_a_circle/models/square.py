198ec93dffec03628eba294564e26c6760c1fd31
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-13 18:49:25 +0000

    initialization

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,4 +10,7 @@ from models.Rectangle import Rectangle
 
 class Square(Rectangle):
     """describes a square"""
-    
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialization"""
+        super().__init__(id, x, y, size, size)

bd5bb92e6510ae2401c4402a9e239b78fb9e00b6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-14 15:57:27 +0000

    validate attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,7 @@ class Square inheriting from Rectangle
 """
 
 
-from models.Rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
@@ -13,7 +13,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """square initialization"""
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
@@ -24,3 +24,34 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Validating attributes"""
+        if args is not None and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if args is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+        elif kwargs is not None and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value

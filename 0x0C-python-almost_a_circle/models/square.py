fb4c44371da15d5b3f2957aeee3dcac441294234
jdev <20902932+osekojoe@users.noreply.github.com>
2023-01-14 15:34:16 +0000

    initialize square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,5 +12,15 @@ class Square(Rectangle):
     """describes a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """initialization"""
+        """square initialization"""
         super().__init__(id, x, y, size, size)
+
+    @property
+    def size(self):
+        """get size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value

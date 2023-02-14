405ef2ec940aa9ef955f4eac6742a02c27dbbb5e
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-14 07:52:18 -0800

    show table description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints full description of first_table from db hbtn_0c_0 in MySQL server
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table;

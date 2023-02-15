5808f9f90e8a05c3224d64e8f25de7bf70e40554
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-15 02:45:53 -0800

    show show-rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL server
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings
+     ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;

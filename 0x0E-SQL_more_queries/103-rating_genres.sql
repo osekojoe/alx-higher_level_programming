20782432fec7ebd81a3e521961d72373b75e5ceb
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-15 02:50:35 -0800

    rank genres

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,2 @@
 -- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC, tv_genres.id ASC;
+SELECT `name`, SUM(`rate`) AS `rating` FROM `tv_genres` AS g INNER JOIN `tv_show_genres` AS s ON s.`genre_id` = g.`id` INNER JOIN `tv_show_ratings` AS r ON r.`show_id` = s.`show_id` GROUP BY `name` ORDER BY `rating` DESC;

5a84acb398f68d8daa4693f98138b2cd3a6aad30
jdev <20902932+osekojoe@users.noreply.github.com>
2023-02-16 05:07:44 -0800

    add comments

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,13 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
 SELECT tv_shows.title FROM tv_shows
 LEFT JOIN
 (
-	SELECT tv_shows.title
-	FROM tv_shows
+	SELECT tv_shows.title FROM tv_shows
      	JOIN tv_show_genres
      	     ON tv_show_genres.show_id = tv_shows.id
      	JOIN tv_genres

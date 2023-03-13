8fbf541ba1b7ff22678ac967d879ec8ca854cdc6
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-13 08:43:47 -0700

    standard compliance

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ let i = parseInt(process.argv[2]);
 if (Number.isNaN(i)) {
 	console.log('Missing number of occurrences');
 } else {
-	while (i > 0) {
-		console.log('C is fun');
-		i--;
-	}
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
+  }
 }

e393374f16b868b2ee2dcf0f287d164d7863d6c9
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-16 09:21:12 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__=='__main__':
     cur = db.cursor()
     cur.execute("SELECT * \
             FROM states \
-            WHERE CONVERT('name' USING Latin1) \
+            WHERE CONVERT(`name` USING Latin1) \
             COLLATE Latin1_General_CS \
             LIKE 'N%';")
     states = cur.fetchall()

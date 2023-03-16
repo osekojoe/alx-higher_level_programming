b3ce7548fcc3d02e44abdc38bbe99aabc2844afe
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-16 09:10:50 -0700

    filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from the
+database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__=='__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * \
+            FROM states \
+            WHERE CONVERT('name' USING Latin1) \
+            COLLATE Latin1_General_CS \
+            LIKE 'N%';")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)

0e4e10aca4a0974e331ff61d1f38de54b9c050d1
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-17 01:34:25 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ hbtn_0e_0_usa where name matches the argument
 import sys
 import MySQLdb
 
-if __name__=='__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()

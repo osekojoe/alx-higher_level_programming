90ff4eb857ff589410b7ed35b7237085ae6c455c
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-17 01:37:10 -0700

    pycodestyle fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,13 +6,13 @@ hbtn_0e_0_usa where name matches the argument
 import sys
 import MySQLdb
 
-if __name__=='__main__':
+if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * \
             FROM states \
-            WHERE name = %s;",(sys.argv[4],))
+            WHERE name = %s;", (sys.argv[4],))
     states = cur.fetchall()
 
     for state in states:

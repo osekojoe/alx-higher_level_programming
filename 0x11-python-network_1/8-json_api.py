1cbeb4f4ae134ce53156d0fb85fd63c1022c0d8e
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-31 06:14:46 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to
+   http://0.0.0.0:5000/search_user with the letter as a parameter.
+    The letter must be sent in the variable q
+    If no argument is given, set q=""
+If the response body is properly JSON formatted and not empty,
+  display the id and name like this: [<id>] <name>
+    Otherwise:
+        Display Not a valid JSON if the JSON is invalid
+        Display No result if the JSON is empty
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {'q': letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

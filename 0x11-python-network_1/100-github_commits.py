da2bd14420f3a266cddbf521b447b889384711dc
jdev <20902932+osekojoe@users.noreply.github.com>
2023-03-31 06:19:31 -0700

    Github API

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo, owner = sys.argv[1], sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        repo, owner)
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print('{}: {}'.format(
+                commits[i].get('sha'),
+                commits[i].get('commit').get('author').get('name')))
+    except IndexError:
+        pass

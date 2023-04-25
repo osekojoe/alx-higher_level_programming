f16c56aa2f68a683ee430ad24cc6e0959ecd9f58
jdev <20902932+osekojoe@users.noreply.github.com>
2023-04-25 17:08:51 +0000

    read and print file contents

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 // Reads and prints the content of a file.
 const fs = require('fs');
 
-
-fs.readFile(process.argv[2], 'utf-8', function(err, data){
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data)
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
